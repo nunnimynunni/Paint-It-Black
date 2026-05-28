9f15e3b33c9e8db9c74e00f50d3b4ed33cc7aca2
nunnimynunni <arribalzagabrenda@gmail.com>
2026-05-27 21:14:41 -0300

    MOVIMIENTO DIAGONALES

--- Assets/Scripts/Playeranimator.cs
+++ Assets/Scripts/Playeranimator.cs
@@ -18,12 +18,27 @@ public class PlayerAnimator : MonoBehaviour
 
         Vector3 movimiento = Vector3.zero;
 
-        if (keyboard.sKey.isPressed)
-            movimiento = Vector3.down;
+        if (keyboard.wKey.isPressed) movimiento += Vector3.up;
+        if (keyboard.sKey.isPressed) movimiento += Vector3.down;
+        if (keyboard.aKey.isPressed) movimiento += Vector3.left;
+        if (keyboard.dKey.isPressed) movimiento += Vector3.right;
 
-        bool isMoving = movimiento != Vector3.zero;
+        // Normalizar para que en diagonal no vaya más rápido
+        if (movimiento.magnitude > 1)
+            movimiento = movimiento.normalized;
 
         transform.Translate(movimiento * velocidad * Time.deltaTime);
+
+        // Animaciones
+        bool isMoving = keyboard.sKey.isPressed;
+        bool yendoDerecha = keyboard.dKey.isPressed && !keyboard.sKey.isPressed;
+        bool yendoArriba = keyboard.wKey.isPressed && !keyboard.dKey.isPressed;
+        bool yendoIzq = keyboard.aKey.isPressed && !keyboard.wKey.isPressed
+                                                     && !keyboard.sKey.isPressed;
+
         anim.SetBool("isMoving", isMoving);
+        anim.SetBool("yendoDerecha", yendoDerecha);
+        anim.SetBool("yendoArriba", yendoArriba);
+        anim.SetBool("yendoIzq", yendoIzq);
     }
 }
