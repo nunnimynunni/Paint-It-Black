1f2344da23608551984764063f52b122672b66ce
nunnimynunni <arribalzagabrenda@gmail.com>
2026-06-24 17:18:08 -0300

    Reapply "tODOS LOS SONIDOS LISTOOOOS"
    This reverts commit 3ee9015b34f9b7d10fbe8f012fea864208f95463.

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -127,6 +127,8 @@ public class EnemyHealth : MonoBehaviour
         currentHP -= amount;
         ultimoColorRecibido = color;
 
+        if (SfxManager.Instance != null) SfxManager.Instance.PlayImpacto();
+
         if (anim != null) anim.SetTrigger("OnHit");
         stunTimer = duracionAturdimientoPorGolpe;
 
